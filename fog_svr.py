090454bd2500396d8464dac2437dfffbaa42d7ac
shaletaweizhu <thinkfirst@126.com>
2018-11-11 12:43:32 +0800

    更新带上了log

--- fog_svr.py
+++ fog_svr.py
@@ -8,14 +8,24 @@ sys.setdefaultencoding('utf-8')
 import socket
 import time
 import threading
+import logging
+from logging.handlers import RotatingFileHandler
 
-cloud_ip = '104.293.100.134'
+cloud_ip = '104.224.166.34'
 cloud_port = 29999
 services = {'22':22,}
 base_port = 40000
 
 server_hb_pkg = '___31415926___1___hb'
 client_hb_pkg = '___31415926___1___chb'
+
+
+logging.basicConfig(level=logging.INFO)
+RLogger = RotatingFileHandler('fog.log', maxBytes=5*1024*1024, backupCount=5)
+RLogger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime) s %(message)s')
+RLogger.setFormatter(formatter)
+logging.getLogger('').addHandler(RLogger)
 
 class SafeSock():
     def __init__(self, port = 0):
@@ -26,6 +36,7 @@ class SafeSock():
         self.port = port
         self.closed = True
         self.last_hb_req_time = 0.0
+        self.read_buffer = ''
 
     def makeConnectedSock(self, ip, port):
         try:
@@ -106,28 +117,34 @@ class SafeDictionary():
             self.dic.pop(key)
 
 
-def sock_pair_read_thread_function(frome,to,read_buf,read_buf_lock,read_buf_sema):
+def sock_pair_read_thread_function(frome,to,read_buf,read_buf_lock,read_buf_sema,seq, logstr):
     while 1:
         if to.closed:
+            logging.error('rthread proxy %s to closed when read frome. %s' % (seq,logstr))
             frome.close()
             break
         else:
             try:
                 data = frome.read()
-                if data:
+                if data and len(data):
                     with read_buf_lock:
                         read_buf.append(data)
                     read_buf_sema.release()
                 else:
+                    logging.error('rthread proxy %s frome close. %s' % (seq,logstr))
                     frome.close()
                     break
             except Exception as e:
+                logging.error('rthread proxy %s frome excption:%s. %s' % (seq, e.message,logstr))
                 frome.close()
                 break
+    read_buf_sema.release()
+    logging.info('rthread proxy %s frome end. %s' % (seq,logstr))
 
-def sock_pair_write_thread_function(to,frome,write_buf,write_buf_lock,write_buf_sema):
+def sock_pair_write_thread_function(to,frome,write_buf,write_buf_lock,write_buf_sema,seq,logstr):
     while 1:
         if frome.closed:
+            logging.error('wthread proxy %s frome closed when write. %s' % (seq,logstr))
             to.close()
             break
         else:
@@ -140,26 +157,28 @@ def sock_pair_write_thread_function(to,frome,write_buf,write_buf_lock,write_buf_
                 if data:
                     to.write(data)
             except Exception as e:
+                logging.error('wthread proxy %s to write excption:%s. %s' % (seq, e.message, logstr))
                 to.close()
                 break
+    logging.info('wthread proxy %s to end. %s' % (seq,logstr))
 
-def start_sock_pair_proxy(local,remote):
+def start_sock_pair_proxy(local,remote,seq):
     client_read_buf = []
     client_read_buf_lock = threading.Lock()
     client_read_buf_semaphore = threading.Semaphore(0)
     svr_read_buf = []
     svr_read_buf_lock = threading.Lock()
     svr_read_buf_semaphore = threading.Semaphore(0)
-    local.settimeout(300)
+    local.settimeout(301)
     remote.settimeout(300)
     client_read_thread = threading.Thread(target=sock_pair_read_thread_function, args=(
-    remote, local, client_read_buf, client_read_buf_lock, client_read_buf_semaphore))
+    remote, local, client_read_buf, client_read_buf_lock, client_read_buf_semaphore,seq,'[from:cloud,to:local]'))
     client_write_thread = threading.Thread(target=sock_pair_write_thread_function, args=(
-    local, remote, client_read_buf, client_read_buf_lock, client_read_buf_semaphore))
+    local, remote, client_read_buf, client_read_buf_lock, client_read_buf_semaphore,seq,'[from:cloud,to:local]'))
     svr_read_thread = threading.Thread(target=sock_pair_read_thread_function,
-                                       args=(local, remote, svr_read_buf, svr_read_buf_lock, svr_read_buf_semaphore))
+                                       args=(local, remote, svr_read_buf, svr_read_buf_lock, svr_read_buf_semaphore,seq,'[from:local,to:cloud]'))
     svr_write_thread = threading.Thread(target=sock_pair_write_thread_function,
-                                        args=(remote, local, svr_read_buf, svr_read_buf_lock, svr_read_buf_semaphore))
+                                        args=(remote, local, svr_read_buf, svr_read_buf_lock, svr_read_buf_semaphore,seq,'[from:local,to:cloud]'))
     client_read_thread.start()
     client_write_thread.start()
     svr_read_thread.start()
@@ -172,14 +191,16 @@ def start_proxy_link_thread_function(seq, local_port):
         remote_link = SafeSock()
         remote_link.makeConnectedSock(cloud_ip, cloud_port)
         if not remote_link.closed:
+            logging.info('start proxy, seq:%s, local:%s'% (seq, local_port))
             remote_link.write('___31415926___2___%s' % seq)
-            start_sock_pair_proxy(local_link, remote_link)
+            start_sock_pair_proxy(local_link, remote_link,seq)
         else:
+            logging.error('start proxy fail, connect remote fail, seq:%s, local:%s' % (seq, local_port))
             local_link.close()
     else:
-        pass
+        logging.error('start proxy fail, connect local fail, seq:%s, local:%s' % (seq, local_port))
 
-def proxy_hb_write_thread_function(lsock):
+def proxy_hb_write_thread_function(lsock,local_port):
     try:
         while 1:
             time.sleep(70)
@@ -187,8 +208,10 @@ def proxy_hb_write_thread_function(lsock):
                 lsock.write(client_hb_pkg)
                 lsock.last_hb_req_time = time.time()
             else:
+                logging.error('fog long link %s, hb want write ,but closed' % local_port)
                 break
     except Exception as e:
+        logging.error('fog long link %s, write hb exception:%s' % (local_port, e.message))
         lsock.close()
 
 def proxy_hb_read_thread_function(lsock, local_port):
@@ -198,6 +221,9 @@ def proxy_hb_read_thread_function(lsock, local_port):
             if not lsock.closed:
                 start_read_time = time.time()
                 data = lsock.read(1024)
+                if len(lsock.read_buffer):
+                    data = lsock.read_buffer + data
+                    lsock.read_buffer = ''
                 if data:
                     while len(data):
                         lsock.last_hb_req_time = 0.0
@@ -213,23 +239,26 @@ def proxy_hb_read_thread_function(lsock, local_port):
                             start_proxy_thread = threading.Thread(target=start_proxy_link_thread_function, args=(sequence,local_port))
                             start_proxy_thread.start()
                         elif len(data) > client_hb_pkg:
+                            logging.error('fog long link %s rcv pkg illegal:%s' % (local_port, data))
                             lsock.close()
                         elif not data.startswith('___31415926___'):
+                            logging.error('fog long link %s rcv pkg illegal:%s' % (local_port, data))
                             lsock.close()
                         else:
-                            pass
+                            lsock.read_buffer = data
                 else:
+                    logging.error('fog long link %s read 0, close' % local_port)
                     lsock.close()
             else:
+                logging.error('fog long link %s read loop closed break' % local_port)
                 break
         except Exception as e:
             if lsock.last_hb_req_time > 1 and start_read_time > lsock.last_hb_req_time:
+                logging.error('fog long link %s hb timeout, e:%s' % (local_port, e.message))
                 lsock.close()
     time.sleep(30)
     t = threading.Thread(target=service_thread_function, args=(local_port,))
     t.start()
-
-
 
 def service_thread_function(port):
     lsock = SafeSock()
@@ -238,20 +267,22 @@ def service_thread_function(port):
         lsock.write('___31415926___0___%s' % (port + base_port))
         try:
             data = lsock.read(64)
+            logging.info('fog long link start, port:%s, rcv data:%s' % (port, data))
             if data and data.startswith('___31415926___0___'):
-                print 'recv data: %s, long link succ' % data
-                w_thread = threading.Thread(target=proxy_hb_write_thread_function, args=(lsock,))
+                w_thread = threading.Thread(target=proxy_hb_write_thread_function, args=(lsock,port))
                 r_thread = threading.Thread(target=proxy_hb_read_thread_function,args=(lsock,port))
                 w_thread.start()
                 r_thread.start()
             else:
                 raise 'remote error'
         except Exception as e:
+            logging.error('fog long link, port:%s start exception:%s' % (port, e.message))
             lsock.close()
             time.sleep(30)
             t = threading.Thread(target=service_thread_function,args=(port,))
             t.start()
     else:
+        logging.error('fog long link,port:%s connect cloud fail' % port)
         time.sleep(30)
         t = threading.Thread(target=service_thread_function, args=(port,))
         t.start()
@@ -259,5 +290,7 @@ def service_thread_function(port):
 
 if __name__ == '__main__':
     for key in services:
+        logging.info('fog start long connection on %s:%s' % (key,services[key]))
         t = threading.Thread(target=service_thread_function,args=(services[key],))
         t.start()
+
